d87cebb42ebec5c301a6b5fd8b300b5b13d30601
sumit8340 <sumit8340.devops@gmail.com>
2025-09-01 17:35:02 +0530

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,34 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      // You don't need to import React in scope when using JSX in React 17+
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+
+  // React plugin recommended config
   pluginReact.configs.flat.recommended,
+
+  // Jest globals for test files
+  {
+    files: ["**/*.test.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        jest: "readonly",
+      },
+    },
+  },
 ]);
